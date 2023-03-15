4008aebb4bf9d83e3cd4898f257e658bf5949d4e
Syed Ali <syedalihamad@hotmail.com>
2023-03-16 02:50:01 +0500

    completed Q2

--- Recursive/ways_2_reach_nth_stairs/Q.go
+++ Recursive/ways_2_reach_nth_stairs/Q.go
@@ -5,7 +5,6 @@ import "log"
 // You have been given number of stairs. Need to reach nth stair each time you can climb 1 or  stairs.
 // retun the number of distinct ways you can climb the stairs
 func nthStair(n int) int {
-
 	if n == 0 {
 		return 1
 	} else if n < 0 {
@@ -16,6 +15,5 @@ func nthStair(n int) int {
 }
 
 func main() {
-
 	log.Println(nthStair(3))
 }
